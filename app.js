09f3db8b0147b1a44e35f5dae4ce8eebd5c3bd87
Hongwei Sun <hongwsun@amazon.com>
2023-09-19 14:56:50 -0500

    add log line for env

--- app.js
+++ app.js
@@ -94,6 +94,6 @@ if (cluster.isMaster) {
     const port = process.env.PORT || 3000;
 
     const server = app.listen(port, function () {
-        console.log('Server running at http://127.0.0.1:' + port + '/');
+        console.log('Server running at http://127.0.0.1:' + port + '/' + 'env variable: secret1:' + process.env.secret1);
     });
 }
